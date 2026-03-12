de32af93193c3a69bcdf666885927ae3179a3b83
moonlightbayapp <moonlightbay10@gmail.com>
2026-03-12 14:23:32 +0700

    Create head-config.js

---
+++ head-config.js
@@ -0,0 +1,10 @@
+const headContent = `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+`;
+
+// Suntikkan ke bagian paling atas <head>
+document.head.insertAdjacentHTML('afterbegin', headContent);
